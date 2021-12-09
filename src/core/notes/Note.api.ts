bdb3b211e22dd9cb933652386523a4090672188e
Gabriel Blake <gabriel.blake28@gmail.com>
2021-12-09 11:05:53 -0500

    note api

---
+++ src/core/notes/Note.api.ts
@@ -0,0 +1,43 @@
+import { Note } from "./Note";
+import Query from "./NoteQuery";
+
+export default class NoteApi {
+  private query: Query = new Query();
+
+  async get(id: string) {
+    try {
+      const payload = await this.query.readById(id);
+
+      if (payload.data) {
+        return payload.data;
+      } else {
+        return new Error(payload.message);
+      }
+    } catch (e: any) {
+      return e;
+    }
+  }
+
+  async put(id: string, resource: Note) {
+    return this.query.putById(id, resource);
+  }
+  async create(resource: Note) {
+    return this.query.create(resource);
+  }
+  async delete(id: string) {
+    return this.query.deleteById(id);
+  }
+  async getAll() {
+    try {
+      const payload = await this.query.list();
+
+      if (payload.data) {
+        return payload.data;
+      } else {
+        return new Error(payload.message);
+      }
+    } catch (e: any) {
+      return e;
+    }
+  }
+}
