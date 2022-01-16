d4b592758c00af30f19e9724b4f87124e731a560
Gabriel Blake <gabriel.blake28@gmail.com>
2022-01-16 00:37:28 -0800

    delete comment button is disabled when user is logged in

--- src/components/Notes/NoteComponent.tsx
+++ src/components/Notes/NoteComponent.tsx
@@ -1,25 +1,29 @@
-import { Box, Text, Button, TextArea } from "grommet";
-import { Edit, Trash } from "grommet-icons";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { updateNote, deleteNote } from "../../core/notes/NoteSlice";
 import useUser from "../Routes/useUser";
-import {
-  IconButton,
-  OutlinedInputProps,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { IconButton, Menu, TextField, Typography } from "@mui/material";
 import DeleteOutlineSharpIcon from "@mui/icons-material/DeleteOutlineSharp";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { alpha, styled } from "@mui/material/styles";
-import { TextFieldProps } from "@mui/material/TextField";
 import "./NotesComponent.css";
+import React from "react";
 
 export default function NoteComponent(props) {
   const [note, setNote] = useState("");
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const dispatch = useDispatch();
   const user = useUser();
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
 
   useEffect(() => setNote(props.note.note), [props.note]);
   useEffect(() => console.log(user), [user]);
@@ -27,10 +31,58 @@ export default function NoteComponent(props) {
     <div>
       <div>
         {/* <Box>
-            <Button
-              hoverIndicator="background"
-              icon={<Edit size="small" color="#708090" />}
-              color="#708090"
+          <Button
+            hoverIndicator="background"
+            icon={<Edit size="small" color="#708090" />}
+            color="#708090"
+            onClick={(e) => {
+              dispatch(
+                updateNote(
+                  Object.assign({}, props.note, {
+                    note: note,
+                    last_modified: new Date(),
+                    modified_by: "user",
+                  })
+                )
+              );
+            }}
+          />
+        </Box>
+        <Box>
+          <Button
+            hoverIndicator="background"
+            onClick={(e) => {
+              dispatch(deleteNote(props.note));
+            }}
+            disabled={user.user_name == "user" ? true : false}
+            icon={<Trash size="small" color="#708090" />}
+            color="#708090"
+          />
+        </Box> */}
+      </div>
+
+      <div>
+        <div className="more-actions-button">
+          <IconButton
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <MoreHorizIcon />
+          </IconButton>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <IconButton
+              size="small"
               onClick={(e) => {
                 dispatch(
                   updateNote(
@@ -42,36 +94,29 @@ export default function NoteComponent(props) {
                   )
                 );
               }}
-            />
-          </Box>
-          <Box>
-            <Button
-              hoverIndicator="background"
+            >
+              <EditOutlinedIcon />
+            </IconButton>
+            <IconButton
+              size="small"
               onClick={(e) => {
                 dispatch(deleteNote(props.note));
               }}
               disabled={user.user_name == "user" ? true : false}
-              icon={<Trash size="small" color="#708090" />}
-              color="#708090"
-            />
-          </Box> */}
-      </div>
-
-      <div>
-        <div className="more-actions-button">
-          <IconButton>
-            <MoreHorizIcon />
-          </IconButton>
+            >
+              <DeleteOutlineSharpIcon />
+            </IconButton>
+          </Menu>
         </div>
         <div className="note-box">
           <TextField
             variant="standard"
             InputProps={{
               disableUnderline: true,
+              readOnly: true,
             }}
             fullWidth
             multiline={true}
-            size="medium"
             value={note}
             onChange={(e) => setNote(e.target.value)}
           />
