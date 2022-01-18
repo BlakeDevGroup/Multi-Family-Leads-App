dcffdd464ae96e326d84b368961e44ca2f662a6c
cdblake1 <cdblake31@gmail.com>
2022-01-18 13:46:52 -0800

    preliminaries

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -1,15 +1,18 @@
-import { Box, Main } from "grommet";
-import NavBar from "../NavBar/NavBar";
+import { Main } from "grommet";
 import { useState } from "react";
-import MainLayer from "../Layer/Layer";
 import FixedSideBarComponent from "../SideBar/FixedSideBar";
 import "./shell.css";
+import { NotificationParameters } from "../../common/notification/def/NotificationParameters";
+import { ReactNotificationService } from "../../common/notification/ui/ReactNotificationService";
+import NotificationProvider from "../../common/notification/ui/NotificationProvider";
 
+const notificationService = new ReactNotificationService();
 export default function Shell({ view }) {
   const [open, setOpen] = useState(false);
 
   return (
     <Main background="#f1f5f8" overflow="hidden">
+      <NotificationProvider id="presenter1" />
       <div className="mfl-shell-container">
         <FixedSideBarComponent />
         <div className="mfl-shell-content-body">{view}</div>
@@ -17,3 +20,16 @@ export default function Shell({ view }) {
     </Main>
   );
 }
+
+setInterval(() => {
+  const session = notificationService.makeNotificationSession(
+    {
+      message: "Some Notification",
+      duration: 2000,
+      loading: true,
+    } as NotificationParameters,
+    "presenter1"
+  );
+
+  if (session.provider.Notifications.length < 10) session.notify();
+}, 1000);
